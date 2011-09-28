c42cce1925e583ba4cd1a47de2417fb841f4774f
Tobias Lorenz <tobias.lorenz@etas.com>
2011-09-28 11:42:33 +0200

    MSBuild script added.
    Signed-off-by: Tobias Lorenz <tobias.lorenz@etas.com>

---
+++ Sources/build.bat
@@ -0,0 +1,17 @@
+@echo off
+
+set DOTNET=C:\Windows\Microsoft.NET\Framework\v4.0.30319
+if exist %DOTNET%\MSBuild.exe goto BUILD
+
+set DOTNET=C:\Windows\Microsoft.NET\Framework\v3.5
+if exist %DOTNET%\MSBuild.exe goto BUILD
+
+echo .NET Framework not found. Build failed!
+goto END
+
+:BUILD
+echo Using MSBuild found in %DOTNET%
+set PATH=%DOTNET%;%PATH%
+MSBuild BUSMASTER.sln /property:Configuration=Release
+
+:END
