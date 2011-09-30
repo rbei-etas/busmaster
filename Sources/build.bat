4123d56906482c576bcdb3048d70cff19aeea038
Tobias Lorenz <tobias.lorenz@etas.com>
2011-09-30 10:52:43 +0200

    Also build TOOLS_EXTERNAL with build.bat.
    Signed-off-by: Tobias Lorenz <tobias.lorenz@etas.com>

--- Sources/build.bat
+++ Sources/build.bat
@@ -13,5 +13,8 @@ goto END
 echo Using MSBuild found in %DOTNET%
 set PATH=%DOTNET%;%PATH%
 MSBuild BUSMASTER.sln /property:Configuration=Release
+MSBuild TOOLS_EXTERNAL\CAPL_2_C\Language_ConverterDSW.sln /property:Configuration=Release
+MSBuild TOOLS_EXTERNAL\EXT_DBC_2_DBF\CANDBConverterDSW.sln /property:Configuration=Release
+MSBuild TOOLS_EXTERNAL\EXT_DBF_2_DBC\CANDBConverterDSW.sln /property:Configuration=Release
 
 :END
