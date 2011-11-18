13c274214cdd3f3ee038dcb16d3bd52f82f55f31
Arunkumar Karri <K_arunkumar_1217@yahoo.com>
2011-11-18 14:00:22 +0530

    Updated the export function list.

--- Sources/CAN_STUB/CAN_STUB_Extern.h
+++ Sources/CAN_STUB/CAN_STUB_Extern.h
@@ -41,34 +41,7 @@ extern "C" {  // only need to export C interface if used by C++ source code
 #endif
 
 /*  Exported function list */
-USAGEMODE HRESULT CAN_STUB_PerformInitOperations(void);
-USAGEMODE HRESULT CAN_STUB_PerformClosureOperations(void);
-USAGEMODE HRESULT CAN_STUB_GetTimeModeMapping(SYSTEMTIME& CurrSysTime, UINT64& TimeStamp, LARGE_INTEGER* QueryTickCount = NULL);
-USAGEMODE HRESULT CAN_STUB_ListHwInterfaces(INTERFACE_HW_LIST& sSelHwInterface, INT& nCount);
-USAGEMODE HRESULT CAN_STUB_SelectHwInterface(const INTERFACE_HW_LIST& sSelHwInterface, INT nCount);
-USAGEMODE HRESULT CAN_STUB_DeselectHwInterface(void);
-USAGEMODE HRESULT CAN_STUB_DisplayConfigDlg(PCHAR& InitData, int& Length);
-USAGEMODE HRESULT CAN_STUB_SetConfigData(PCHAR pInitData, int Length);
-USAGEMODE HRESULT CAN_STUB_StartHardware(void);
-USAGEMODE HRESULT CAN_STUB_StopHardware(void);
-USAGEMODE HRESULT CAN_STUB_ResetHardware(void);
-USAGEMODE HRESULT CAN_STUB_GetTxMsgBuffer(BYTE*& pouFlxTxMsgBuffer);
-USAGEMODE HRESULT CAN_STUB_SendMsg(DWORD dwClientID, const STCAN_MSG& sCanTxMsg);
-USAGEMODE HRESULT CAN_STUB_GetBoardInfo(s_BOARDINFO& BoardInfo);
-USAGEMODE HRESULT CAN_STUB_GetBusConfigInfo(BYTE* BusInfo);
-USAGEMODE HRESULT CAN_STUB_GetVersionInfo(VERSIONINFO& sVerInfo);
-USAGEMODE HRESULT CAN_STUB_GetLastErrorString(CHAR* acErrorStr, int nLength);
-USAGEMODE HRESULT CAN_STUB_FilterFrames(FILTER_TYPE FilterType, TYPE_CHANNEL Channel, UINT* punMsgIds, UINT nLength);
-USAGEMODE HRESULT CAN_STUB_GetControllerParams(LONG& lParam, UINT nChannel, ECONTR_PARAM eContrParam);
-USAGEMODE HRESULT CAN_STUB_GetErrorCount(SERROR_CNT& sErrorCnt, UINT nChannel, ECONTR_PARAM eContrParam);
-
-// Specific function set
-USAGEMODE HRESULT CAN_STUB_LoadDriverLibrary(void);
-USAGEMODE HRESULT CAN_STUB_SetAppParams(HWND hWndOwner, Base_WrapperErrorLogger* pILog);
-USAGEMODE HRESULT CAN_STUB_UnloadDriverLibrary(void);
-USAGEMODE HRESULT CAN_STUB_ManageMsgBuf(BYTE bAction, DWORD ClientID, CBaseCANBufFSE* pBufObj);
-USAGEMODE HRESULT CAN_STUB_RegisterClient(BOOL bRegister, DWORD& ClientID, TCHAR* pacClientName);
-USAGEMODE HRESULT CAN_STUB_GetCntrlStatus(const HANDLE& hEvent, UINT& unCntrlStatus);
+USAGEMODE HRESULT GetIDIL_CAN_Controller(void** ppvInterface);
 
 
 
