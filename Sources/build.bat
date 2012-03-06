e192e1d7ff20b7ab125852708543f41e25e11379
Tobias Lorenz <tobias.lorenz@etas.com>
2012-03-06 21:24:25 +0100

    Cleanups of Format Converters.
    Signed-off-by: Tobias Lorenz <tobias.lorenz@etas.com>

--- Sources/build.bat
+++ Sources/build.bat
@@ -13,8 +13,6 @@ goto END
 echo Using MSBuild found in %DOTNET%
 set PATH=%DOTNET%;%PATH%
 MSBuild BUSMASTER.sln /property:Configuration=Release
-MSBuild TOOLS_EXTERNAL\CAPL_2_C\CAPL_2_C.sln /property:Configuration=Release
-MSBuild TOOLS_EXTERNAL\DBC_2_DBF\DBC_2_DBF.sln /property:Configuration=Release
-MSBuild TOOLS_EXTERNAL\DBF_2_DBC\DBF_2_DBC.sln /property:Configuration=Release
+MSBuild "Format Converter\FormatConverter.sln" /property:Configuration=Release
 
 :END
