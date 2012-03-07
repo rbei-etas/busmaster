2d1a460f28810459b0fb620f9d798236cc68533c
Tobias Lorenz <tobias.lorenz@etas.com>
2012-03-07 17:35:30 +0100

    Further cleanups.
    Signed-off-by: Tobias Lorenz <tobias.lorenz@etas.com>

--- Sources/CAN_Kvaser_CAN/GetKvaserLib.bat
+++ Sources/CAN_Kvaser_CAN/GetKvaserLib.bat
@@ -1,8 +1,5 @@
 @ECHO OFF
 :: delims is a TAB followed by a space
-FOR /F "tokens=2* delims=	 " %%A IN ('REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\KVASER AB\CanlibSDK"') DO SET Country=%%B
-set str=%Country%
-set str=%str:~7%
-set str=%str%\Lib\MS\canlib32.lib
-echo.%str%
-copy "%str%" "%TMP%\canlib32.lib"
+FOR /F "tokens=2* delims=	 " %%A IN ('REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\KVASER AB\CanlibSDK"') DO SET dir=%%B
+
+copy "%dir%\Lib\MS\canlib32.lib" "%TMP%\canlib32.lib"
