bf9db7ca5c9721603ce30b82a2d286b5befd7004
Arunkumar Karri <K_arunkumar_1217@yahoo.com>
2012-03-14 15:04:56 +0530

    Updated to support both windows XP and Windows 7.

--- Sources/CAN_Kvaser_CAN/GetKvaserLib.bat
+++ Sources/CAN_Kvaser_CAN/GetKvaserLib.bat
@@ -1,5 +1,12 @@
 @ECHO OFF
 :: delims is a TAB followed by a space
-FOR /F "tokens=2* delims=   " %%A IN ('REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\KVASER AB\CanlibSDK"') DO SET dir=%%B
+FOR /F "tokens=2* delims=   " %%A IN ('REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\KVASER AB\CanlibSDK"') DO SET DirPath=%%B
+set str=%DirPath%
+::check for Windows Version 
+systeminfo | find "OS Name" > %TEMP%\osname.txt
+FOR /F "usebackq delims=: tokens=2" %%i IN (%TEMP%\osname.txt) DO set vers=%%i
 
-copy "%dir:~7%\Lib\MS\canlib32.lib" "%TMP%\canlib32.lib"
+echo %vers% | find "Windows XP" > nul
+if %ERRORLEVEL% == 0 set str=%str:~7%
+
+copy "%str%\Lib\MS\canlib32.lib" "%TMP%\canlib32.lib"
