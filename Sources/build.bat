3dbc5f5c8d3d25ea892279784e8d0d3e9a9ae458
Tobias Lorenz <tobias.lorenz@etas.com>
2012-03-17 15:28:35 +0100

    Use %ProgramFiles% and %SystemRoot% to get rid of fixed path.
    Signed-off-by: Tobias Lorenz <tobias.lorenz@etas.com>

--- Sources/build.bat
+++ Sources/build.bat
@@ -1,10 +1,10 @@
 @echo off
 
-set DOTNET=C:\Windows\Microsoft.NET\Framework\v4.0.30319
-if exist %DOTNET%\MSBuild.exe goto BUILD
+set DOTNET=%SystemRoot%\Microsoft.NET\Framework\v4.0.30319
+if exist "%DOTNET%\MSBuild.exe" goto BUILD
 
-set DOTNET=C:\Windows\Microsoft.NET\Framework\v3.5
-if exist %DOTNET%\MSBuild.exe goto BUILD
+set DOTNET=%SystemRoot%\Microsoft.NET\Framework\v3.5
+if exist "%DOTNET%\MSBuild.exe" goto BUILD
 
 echo .NET Framework not found. Build failed!
 goto END
