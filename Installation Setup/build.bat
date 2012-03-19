809d9c553ed99d944949faa834f3aff5dca45932
Tobias Lorenz <tobias.lorenz@etas.com>
2012-03-19 01:20:34 +0100

    Auto generate Changelog.txt as part of NSIS.
    Signed-off-by: Tobias Lorenz <tobias.lorenz@etas.com>

--- Installation Setup/build.bat
+++ Installation Setup/build.bat
@@ -1,5 +1,8 @@
 @echo off
 
-set PATH=%PATH%;%ProgramFiles%\NSIS
+set PATH=%PATH%;%ProgramFiles%\NSIS;%ProgramFiles%\Git\bin
+
+git log > ..\Sources\BIN\Release\Changelog.log
+dos2unix --u2d ..\Sources\BIN\Release\Changelog.log
 
 makensis "BUSMASTER INSTALL SCRIPT.nsi"
