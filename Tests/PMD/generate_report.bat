1d6d0489ff659f8fb5a2ea71a74b9e56a1a7b6fb
Tobias Lorenz <tobias.lorenz@etas.com>
2012-03-31 22:09:06 +0200

    Get PMD tool to work.
    Signed-off-by: Tobias Lorenz <tobias.lorenz@etas.com>

--- Tests/PMD/generate_report.bat
+++ Tests/PMD/generate_report.bat
@@ -1,5 +1,13 @@
 @echo off
 
+:JAVA_FIND
+for /d %%i in ("%ProgramFiles%\Java\jdk*") do set JAVA_HOME=%%i
+if exist "%JAVA_HOME%\lib\tools.jar" goto SERNA_FIND
+
+:JAVA_NOT_FOUND
+echo Java Development Kit not found. Build failed!
+goto END
+
 :SERNA_FIND
 for /d %%i in ("%ProgramFiles%\Syntext\Serna Free*") do set SERNA_HOME=%%i
 if exist "%SERNA_HOME%" goto DITA_FIND
@@ -17,6 +25,11 @@ echo DITA not found. Build failed!
 goto END
 
 :DITA_1_5
+set CLASSPATH=%CLASSPATH%;%DITA_HOME%\lib
+set CLASSPATH=%CLASSPATH%;%DITA_HOME%\lib\dost.jar
+set CLASSPATH=%CLASSPATH%;%DITA_HOME%\lib\commons-codec-1.4.jar
+set CLASSPATH=%CLASSPATH%;%DITA_HOME%\lib\resolver.jar
+set CLASSPATH=%CLASSPATH%;%DITA_HOME%\lib\icu4j.jar
 set CLASSPATH=%CLASSPATH%;%DITA_HOME%\lib\saxon
 set CLASSPATH=%CLASSPATH%;%DITA_HOME%\lib\saxon\saxon9.jar
 set CLASSPATH=%CLASSPATH%;%DITA_HOME%\lib\saxon\saxon9-dom.jar
@@ -27,20 +40,42 @@ set CLASSPATH=%CLASSPATH%;%DITA_HOME%\lib\saxon\saxon9-sql.jar
 set CLASSPATH=%CLASSPATH%;%DITA_HOME%\lib\saxon\saxon9-xom.jar
 set CLASSPATH=%CLASSPATH%;%DITA_HOME%\lib\saxon\saxon9-xpath.jar
 set CLASSPATH=%CLASSPATH%;%DITA_HOME%\lib\saxon\saxon9-xqj.jar
-set OPTS=-Xmx512m
+set ANT_OPTS=-Xmx512m
+set ANT_OPTS=%ANT_OPTS% -Djavax.xml.transform.TransformerFactory=net.sf.saxon.TransformerFactoryImpl
 
 :PMD_FIND
 for /d %%i in ("%ProgramFiles%\PMD*") do set PMD_HOME=%%i
-if exist "%PMD_HOME%" goto CPD
+if exist "%PMD_HOME%" goto MINGW
 
 :PMD_NOT_FOUND
 echo PMD not found. Build failed!
 goto END
 
-:CPD
+:MINGW
+set MINGW_HOME=C:\MinGW
+if exist "%MINGW_HOME%" goto MSYS
+
+:MINGW_NOT_FOUND
+echo MinGW not found. Build failed!
+goto END
+
+:MSYS
+set MSYS_HOME=%MINGW_HOME%\msys\1.0
+if exist "%MSYS_HOME%" goto BUILD
+
+:MSYS_NOT_FOUND
+echo MSYS not found. Build failed!
+goto END
+
+:BUILD
+echo Using Java found in %JAVA_HOME%
 echo Using PMD found in %PMD_HOME%
-set ARGS=--minimum-tokens 75 --files ..\..\Sources --language cpp --format xml --encoding utf-8
-java %OPTS% -Djava.ext.dirs="%PMD_HOME%\lib" net.sourceforge.pmd.cpd.CPD %ARGS% > cpd.xml
+echo Using MSYS found in %MSYS_HOME%
+set ANT_HOME=%DITA_HOME%\tools\ant
+set PATH=%JAVA_HOME%\bin;%ANT_HOME%\bin;%MSYS_HOME%\bin;%PATH%
+set CLASSPATH=%CLASSPATH%;%PMD_HOME%\lib\pmd-5.0-alpha.jar
+call ant -Dpmd.dir="%PMD_HOME%" -v -f build.xml
 
 :END
+pause
 exit 0
