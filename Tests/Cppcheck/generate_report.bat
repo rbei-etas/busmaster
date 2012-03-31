3fd7c3d2f11ae933a675d4c4087a939d84b1e973
Tobias Lorenz <tobias.lorenz@etas.com>
2012-03-31 18:47:23 +0200

    Let Cppcheck generate HTML reports. Suppress certain files.
    Signed-off-by: Tobias Lorenz <tobias.lorenz@etas.com>

--- Tests/Cppcheck/generate_report.bat
+++ Tests/Cppcheck/generate_report.bat
@@ -2,16 +2,30 @@
 
 :CPPCHECK_FIND
 set CPPCHECK_HOME=%ProgramFiles%\Cppcheck
-if exist "%CPPCHECK_HOME%" goto BUILD
+if exist "%CPPCHECK_HOME%" goto PYTHON_FIND
 
 :CPPCHECK_NOT_FOUND
 echo Cppcheck not found. Build failed!
 goto END
 
+:PYTHON_FIND
+set PYTHON_HOME=C:\Python27
+if exist "%PYTHON_HOME%" goto BUILD
+set PYTHON_HOME=C:\Python32
+if exist "%PYTHON_HOME%" goto BUILD
+
+:PYTHON_NOT_FOUND
+echo Python not found. Build failed!
+goto END
+
 :BUILD
 echo Using Cppcheck found in %CPPCHECK_HOME%
 set PATH=%PATH%;%CPPCHECK_HOME%
-cppcheck --xml ../../Sources 2> cppcheck.xml
+cppcheck --enable=all --force --suppressions-list=suppressions.txt --xml ../../Sources 2> cppcheck.xml
+
+:HTMLREPORT
+set PATH=%PATH%;%PYTHON_HOME%
+python "%CPPCHECK_HOME%\htmlreport\cppcheck-htmlreport" --file=cppcheck.xml --report-dir=report --source-dir=.
 
 :END
 exit 0
