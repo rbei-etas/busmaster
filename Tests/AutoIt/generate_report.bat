f7e740a9298b74b6d02e068ba7a41efbeeed7745
Arunkumar Karri <K_arunkumar_1217@yahoo.com>
2012-04-11 15:57:58 +0200

    Reverted Changes by Dr. Tobias Lorenz

--- Tests/AutoIt/generate_report.bat
+++ Tests/AutoIt/generate_report.bat
@@ -2,19 +2,41 @@
 
 set PATH=%PATH%;%ProgramFiles%\AutoIt3
 
-REM Installer
-cd Installer
+
+:INSTALL
+set TESTGROUP=Installer
+echo %TESTGROUP%
+cd %TESTGROUP%
+
 set TEST=TS_XX_install_full
 echo %TEST%
 call autoit3 %TEST%.au3 > %TEST%_out.txt 2> %TEST%_err.txt
+
 cd ..
 
-REM Other Tests
+
+:TESTS
+set TESTGROUP=Format Converter
+echo %TESTGROUP%
+cd %TESTGROUP%
+
+set TEST=TS_XX_DBC2DBF_001
+echo %TEST%
+call autoit3 %TEST%.au3 > %TEST%_out.txt 2> %TEST%_err.txt
+
+cd ..
 
 
-REM Uninstaller
-cd Installer
+:UNINSTALL
+set TESTGROUP=Installer
+echo %TESTGROUP%
+cd %TESTGROUP%
+
 set TEST=TS_XX_uninstall
 echo %TEST%
 call autoit3 %TEST%.au3 > %TEST%_out.txt 2> %TEST%_err.txt
+
 cd ..
+
+
+:END
