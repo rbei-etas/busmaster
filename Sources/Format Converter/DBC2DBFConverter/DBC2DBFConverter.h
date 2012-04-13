d02d86ffdd27d8f6353fc1b6823df7556103a9f5
Tobias Lorenz <tobias.lorenz@etas.com>
2012-04-13 15:59:32 +0200

    Astyle run over DBC2DBF Converter
    Signed-off-by: Tobias Lorenz <tobias.lorenz@etas.com>

--- Sources/Format Converter/DBC2DBFConverter/DBC2DBFConverter.h
+++ Sources/Format Converter/DBC2DBFConverter/DBC2DBFConverter.h
@@ -35,11 +35,13 @@ public:
     virtual HRESULT GetInputFileFilters(string&, string&);
     virtual HRESULT GetOutputFileFilters(string&, string&);
     virtual HRESULT ConvertFile(string& chInputFile, string& chOutputFile);
-    virtual HRESULT GetConverterName(string& strConverterName); 
+    virtual HRESULT GetConverterName(string& strConverterName);
     virtual HRESULT GetErrorStatus(HRESULT hResult, string& omstrStatus);
     virtual HRESULT GetLastConversionStatus(HRESULT& hResult, string& omstrStatus);
     virtual HRESULT GetHelpText(string& pchHelpText);
     virtual BOOL bHaveOwnWindow();
-    virtual HRESULT GetPropertyPage(CPropertyPage*& pPage){return S_FALSE;};
+    virtual HRESULT GetPropertyPage(CPropertyPage*& pPage) {
+        return S_FALSE;
+    };
     ~CDBC2DBFConverter(void);
 };
