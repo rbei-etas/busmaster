ae9d3a7a9c589bf67dc6798e2bdf620141091cdc
Tobias Lorenz <tobias.lorenz@etas.com>
2012-04-14 17:57:45 +0200

    Add missing file headers.
    Signed-off-by: Tobias Lorenz <tobias.lorenz@etas.com>

--- Sources/Format Converter/FormatConverterApp/FormatConverterApp.h
+++ Sources/Format Converter/FormatConverterApp/FormatConverterApp.h
@@ -1,5 +1,26 @@
-// FormatConverter.h : main header file for the PROJECT_NAME application
-//
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * \file      FormatConverterApp.h
+ * \brief     Main header file for the Format Converter application
+ * \author    Tobias Lorenz
+ * \copyright Copyright (c) 2011, Robert Bosch Engineering and Business Solutions. All rights reserved.
+ *
+ * Main header file for the Format Converter application
+ */
 
 #pragma once
 
