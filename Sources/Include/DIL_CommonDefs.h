59f3ad8e5fd6dd27fac485724e6197e21b3b4dee
Tobias Lorenz <tobias.lorenz@etas.com>
2012-05-04 13:30:43 +0200

    Cleanups in DIL_CommonDefs.h.
    Signed-off-by: Tobias Lorenz <tobias.lorenz@etas.com>

--- Sources/Include/DIL_CommonDefs.h
+++ Sources/Include/DIL_CommonDefs.h
@@ -16,7 +16,7 @@
 /**
  * \file      DIL_CommonDefs.h
  * \brief     Some macro definitions necessary
- * \authors   Ratnadip Choudhury, Pradeep Kadoor
+ * \authors   Ratnadip Choudhury, Pradeep Kadoor, Tobias Lorenz
  * \copyright Copyright (c) 2011, Robert Bosch Engineering and Business Solutions. All rights reserved.
  *
  * Some macro definitions necessary
@@ -24,24 +24,11 @@
 
 #pragma once
 
-/* Common DIL macros starts*/
-#include "DataTypes/DIL_Datatypes.h"
-
-static bool bIs_NULL_Pointer(char* /*PtrName*/, void* Ptr)
-{
-    return (Ptr == NULL);
-}
-
 #define LOG_ERR_MSG() sg_pIlog->vLogAMessage(A2T(__FILE__), __LINE__, A2T((LPSTR) (sg_acErrStr.c_str())))
 
-#define VALIDATE_POINTER_RETURN_VAL(Ptr, RetVal) if (bIs_NULL_Pointer(#Ptr, Ptr)) {return RetVal;}
+#define VALIDATE_POINTER_RETURN_VAL(Ptr, RetVal)        if (Ptr == NULL) {return RetVal;}
 #define VALIDATE_VALUE_RETURN_VAL(Val1, Val2, RetVal)   if (Val1 != Val2) {return RetVal;}
-#define VALIDATE_POINTER_RETURN_VOID(Ptr)               if (bIs_NULL_Pointer(#Ptr, Ptr)) {return;}
-#define VALIDATE_POINTER_NO_RETURN_LOG(Ptr)             if (bIs_NULL_Pointer(#Ptr, Ptr)) {LOG_ERR_MSG();}
-#define VALIDATE_POINTER_RETURN_VOID_LOG(Ptr)           if (bIs_NULL_Pointer(#Ptr, Ptr)) {LOG_ERR_MSG(); return;}
-#define VALIDATE_POINTER_RETURN_VALUE_LOG(Ptr, RetVal)  if (bIs_NULL_Pointer(#Ptr, Ptr)) {LOG_ERR_MSG(); return RetVal;}
-
-//#define INITIALISE_DATA(Data)   memset(&Data, 0, sizeof(Data))
-//#define INITIALISE_ARRAY(Array) memset(Array, 0, sizeof(Data))
-#define MAX_HW_INTERFACE 4
-/* Common DIL macros ends*/
+#define VALIDATE_POINTER_RETURN_VOID(Ptr)               if (Ptr == NULL) {return;}
+#define VALIDATE_POINTER_NO_RETURN_LOG(Ptr)             if (Ptr == NULL) {LOG_ERR_MSG();}
+#define VALIDATE_POINTER_RETURN_VOID_LOG(Ptr)           if (Ptr == NULL) {LOG_ERR_MSG(); return;}
+#define VALIDATE_POINTER_RETURN_VALUE_LOG(Ptr, RetVal)  if (Ptr == NULL) {LOG_ERR_MSG(); return RetVal;}
