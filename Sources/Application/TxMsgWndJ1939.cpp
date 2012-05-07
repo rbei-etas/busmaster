2385742e1f2aad5562bd4246e9e0b82e67d1fd6d
Arunkumar Karri <K_arunkumar_1217@yahoo.com>
2012-05-07 17:40:10 +0530

    Addition of function to automatically adjust the width of PGN ID combobox.

--- Sources/Application/TxMsgWndJ1939.cpp
+++ Sources/Application/TxMsgWndJ1939.cpp
@@ -110,6 +110,50 @@ void CTxMsgWndJ1939::vPopulatePGNComboBox(void)
     m_omComboPGN.SetHorizontalExtent(omSize.cx);
     m_omComboPGN.SetCurSel(0);
     m_CS_ConfigData.Unlock();
+    vAdjustWidthMessageComboBox();
+}
+
+/**
+ * \brief Adjust Message Combo Box Width
+ *
+ * This function adjusts the width of the Message Combo Box
+ * according to its contents.
+ */
+void CTxMsgWndJ1939::vAdjustWidthMessageComboBox()
+{
+    CString str;
+    CSize sz;
+    int dx = 0;
+    TEXTMETRIC tm;
+    CDC* pDC = m_omComboPGN.GetDC();
+    CFont* pFont = m_omComboPGN.GetFont();
+
+    // Select the listbox font, save the old font
+    CFont* pOldFont = pDC->SelectObject(pFont);
+    // Get the text metrics for avg char width
+    pDC->GetTextMetrics(&tm);
+
+    for (int i = 0; i < m_omComboPGN.GetCount(); i++)
+    {
+        m_omComboPGN.GetLBText(i, str);
+        sz = pDC->GetTextExtent(str);
+        sz = pDC->GetTextExtent(str);
+
+        // Add the avg width to prevent clipping
+        sz.cx += tm.tmAveCharWidth;
+        if (sz.cx > dx)
+            dx = sz.cx;
+    }
+
+    // Select the old font back into the DC
+    pDC->SelectObject(pOldFont);
+    m_omComboPGN.ReleaseDC(pDC);
+
+    // Adjust the width for the vertical scroll bar and the left and right border.
+    dx += ::GetSystemMetrics(SM_CXVSCROLL) + 2*::GetSystemMetrics(SM_CXEDGE);
+
+    // Set the width of the list box so that every item is completely visible.
+    m_omComboPGN.SetDroppedWidth(dx);
 }
 
 const int NO_OF_CHAR_IN_BYTE = 2;
