f7768d72613e4ab8327f1f80bdc06b1ef90a3f73
Tobias Lorenz <tobias.lorenz@etas.com>
2012-05-08 15:58:42 +0200

    Doxygen improvements.
    Signed-off-by: Tobias Lorenz <tobias.lorenz@etas.com>

--- Sources/Utility/ComputeCheckSum.cpp
+++ Sources/Utility/ComputeCheckSum.cpp
@@ -16,7 +16,7 @@
 /**
  * \file      ComputeCheckSum.cpp
  * \brief     This file contains implementation of CComputeCheckSum class
- * \author    Anish Kumar
+ * \author    Anish Kumar, Tobias Lorenz
  * \copyright Copyright (c) 2011, Robert Bosch Engineering and Business Solutions. All rights reserved.
  *
  * This file contains implementation of CComputeCheckSum class
@@ -25,51 +25,37 @@
 #include "Utils_stdafx.h"
 #include "ComputeCheckSum.h"    // For CComputeCheckSum class definition
 
-
 #define defSIZE_OF_ERROR_BUFFER 1024
 #define defPROJECT_NAME         _T("MCNet")
-/*******************************************************************************
-  Function Name  : CMDIChildBase
-  Description    : Standard default constructor
-  Member of      : CComputeCheckSum
-  Functionality  :  -
-  Author(s)      :
-  Date Created   :
-  Modifications  :
-*******************************************************************************/
 
+/**
+ * \brief Constructor
+ *
+ * Standard default constructor
+ */
 CComputeCheckSum::CComputeCheckSum()
 {
 }
 
-/*******************************************************************************
-  Function Name  : ~CComputeCheckSum
-  Description    : Standard Destructor
-  Member of      : CComputeCheckSum
-  Functionality  : -
-  Author(s)      :
-  Date Created   :
-  Modifications  :
-*******************************************************************************/
-
+/**
+ * \brief Destructor
+ *
+ * Standard Destructor
+ */
 CComputeCheckSum::~CComputeCheckSum()
 {
 }
-/******************************************************************************/
-/*  Function Name    :  bComputeCheckSum                                      */
-/*  Input(s)         :  pucBuff : pointer to buffer,                          */
-/*                      pucCheckSum :pointer to checksum byte                 */
-/*                      dwSize : number of bytes for which checksum to be     */
-/*                               be computed                                  */
-/*  Output           :  BOOL                                                  */
-/*  Functionality    :  This method computes the checksum for a give bytes    */
-/*                      of data passed as pointer pucBuff. It return TRUE on  */
-/*                      success. The paramter pucCheckSum contains computed   */
-/*                      chechsum value.                                       */
-/*  Member of        :  CComputeCheckSum                                      */
-/*  Friend of        :      -                                                 */
-/*                                                                            */
-/******************************************************************************/
+
+/**
+ * \brief      Compute Check Sum
+ * \param[in]  pucBuff pointer to buffer
+ * \param[in]  dwSize number of bytes for which checksum to be computed
+ * \param[out] pucCheckSum pointer to computed checksum value byte
+ * \return     It return TRUE on success.
+ *
+ * This method computes the checksum for a give bytes
+ * of data passed as pointer pucBuff.
+ */
 BOOL CComputeCheckSum::bComputeCheckSum(UCHAR* pucBuff,
                                         DWORD dwSize,
                                         UCHAR* pucCheckSum)
@@ -100,21 +86,17 @@ BOOL CComputeCheckSum::bComputeCheckSum(UCHAR* pucBuff,
     return bReturn;
 }
 
-/******************************************************************************/
-/*  Function Name    :  bGetCheckSum                                          */
-/*  Input(s)         :  omStrConfigFileName : reference to File name          */
-/*                      pucCheckSum : Returns computed check sum              */
-/*                      pucCheckSumInFile : Returns the chechsum in file      */
-/*  Output           :  BOOL                                                  */
-/*  Functionality    :  This method computes the checksum for a given bytes   */
-/*                      in a file. The file is opened and checksum is computed*/
-/*                      for all bytes except the last byte. Returns TRUE for  */
-/*                      sucess and FALSE for failure.                         */
-/*                      chechsum value.                                       */
-/*  Member of        :  CComputeCheckSum                                      */
-/*  Friend of        :      -                                                 */
-/*                                                                            */
-/******************************************************************************/
+/**
+ * \brief      Get Check Sum
+ * \param[in]  omStrConfigFileName reference to File name
+ * \param[out] pucCheckSum Returns computed check sum
+ * \param[out] pucCheckSumInFile : Returns the chechsum in file
+ * \return     TRUE for success and FALSE for failure
+ *
+ * This method computes the checksum for a given bytes
+ * in a file. The file is opened and checksum is computed
+ * for all bytes except the last byte.
+ */
 BOOL CComputeCheckSum::bGetCheckSum(CString& omStrConfigFileName,
                                     UCHAR* pucCheckSum, UCHAR* pucCheckSumInFile)
 {
@@ -188,20 +170,17 @@ BOOL CComputeCheckSum::bGetCheckSum(CString& omStrConfigFileName,
     return bReturn;
 }
 
-/******************************************************************************/
-/*  Function Name    :  bSetCheckSum                                          */
-/*  Input(s)         :  omStrConfigFileName : reference to File name          */
-/*                      pucCheckSum : Returns computed check sum              */
-/*  Output           :  BOOL                                                  */
-/*  Functionality    :  This method computes the checksum for a given bytes   */
-/*                      in a file. The file is opened and checksum is computed*/
-/*                      for all bytes except the last byte. Returns TRUE for  */
-/*                      sucess and FALSE for failure. The same checksum is    */
-/*                      overwritten at the last byte in file.                 */
-/*                      chechsum value.                                       */
-/*  Member of        :  CComputeCheckSum                                      */
-/*  Friend of        :      -                                                 */
-/******************************************************************************/
+/**
+ * \brief      Set Check Sum
+ * \param[in]  omStrConfigFileName reference to File name
+ * \param[out] pucCheckSum Returns computed check sum
+ * \return     Returns TRUE for sucess and FALSE for failure.
+ *
+ * This method computes the checksum for a given bytes
+ * in a file. The file is opened and checksum is computed
+ * for all bytes except the last byte. The same checksum is
+ * overwritten at the last byte in fileSuccess.
+ */
 BOOL CComputeCheckSum::bSetCheckSum(CString& omStrConfigFileName,
                                     UCHAR* pucCheckSum)
 {
@@ -288,22 +267,17 @@ BOOL CComputeCheckSum::bSetCheckSum(CString& omStrConfigFileName,
     return bReturn;
 }
 
-/*****************************************************************************
-  Function Name    :  COM_bSetCheckSum
-  Input(s)         :  omStrConfigFileName : reference to File name
-                      pucCheckSum : Returns computed check sum
-  Output           :  BOOL
-  Functionality    :  This method computes the checksum for a given bytes
-                      in a file. The file is opened and checksum is computed
-                      for all bytes except the last byte. Returns TRUE for
-                      sucess and FALSE for failure. The same checksum is
-                      overwritten at the last byte in file. For COM Support
-  Member of        :  CComputeCheckSum
-  Friend of        :      -
-  Author           : Anish kr
-  Creation Date    : 6/05/10
-  Modifications    :
-******************************************************************************/
+/**
+ * \brief      Set Check Sum
+ * \param[in]  omStrConfigFileName reference to File name
+ * \param[out] pucCheckSum Returns computed check sum
+ * \return     Returns TRUE for sucess and FALSE for failure
+ *
+ * This method computes the checksum for a given bytes
+ * in a file. The file is opened and checksum is computed
+ * for all bytes except the last byte. The same checksum is
+ * overwritten at the last byte in file. For COM Support
+ */
 BOOL CComputeCheckSum::COM_bSetCheckSum(CString& omStrConfigFileName,
                                         UCHAR* pucCheckSum, CString& strError)
 {
