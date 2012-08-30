06fcce6a8afae6b632ad863d8fa615b9a6bc89cc
PGA3KOR <PGA3KOR@KOR317869.KOR.APAC.BOSCH.COM>
2012-08-30 19:50:50 +0530

    Updated Version information to 1.6.7

--- Sources/Format Converter/DBC2DBFConverterLibrary/DBFConverter.cpp
+++ Sources/Format Converter/DBC2DBFConverterLibrary/DBFConverter.cpp
@@ -1114,7 +1114,7 @@ bool CDBFConverter::WriteToOutputFile(fstream& fileOutput)
     fileOutput << endl;
 
     //For easy replacement of version Info #define is not added
-    fileOutput<< "[BUSMASTER_VERSION] [1.6.6]"<<endl;
+    fileOutput<< "[BUSMASTER_VERSION] [1.6.7]"<<endl;
 
     // number of messages
     fileOutput << T_NUM_OF_MSG " " << dec << m_listMessages.size() << endl;
