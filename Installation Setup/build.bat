810ce5146e4f70d12141a8bd82eb8f76e31d72f9
Tobias Lorenz <tobias.lorenz@etas.com>
2013-04-03 16:12:00 +0200

    Cleanup of NSIS files.
    Results of the compilation process should not be part of the repository.
    Thus BUSMASTER_Cleanup_Registry.exe was removed. It can be generated
    by compiling CleanRegistry.msi. This is done now by the following...
    
    The build.bat has been extended to cope with 64-bit Windows versions.
    The compilation of CleanRegistry.nsi was build into the script to
    generate BUSMASTER_Cleanup_Registry.exe automatically.
    
    Signed-off-by: Tobias Lorenz <tobias.lorenz@etas.com>

--- Installation Setup/build.bat
+++ Installation Setup/build.bat
@@ -1,8 +1,33 @@
 @echo off
 
-set PATH=%PATH%;%ProgramFiles%\NSIS;%ProgramFiles%\Git\bin
+:GIT_FIND
+set GIT_HOME=%ProgramFiles%\Git
+if exist "%GIT_HOME%\bin\git.exe" goto NSIS_FIND
+set GIT_HOME=%ProgramFiles(x86)%\Git
+if exist "%GIT_HOME%\bin\git.exe" goto NSIS_FIND
 
+:GIT_NOT_FOUND
+echo Git not found. Build failed!
+goto END
+
+:NSIS_FIND
+set NSIS_HOME=%ProgramFiles%\NSIS
+if exist "%NSIS_HOME%\makensis.exe" goto BUILD
+set NSIS_HOME=%ProgramFiles(x86)%\NSIS
+if exist "%NSIS_HOME%\makensis.exe" goto BUILD
+
+:NSIS_NOT_FOUND
+echo NSIS not found. Build failed!
+goto END
+
+:BUILD
+echo Using git found in %GIT_HOME%
+echo Using NSIS found in $NSIS_HOME%
+set PATH=%PATH%;%GIT_HOME%\bin;%NSIS_HOME%
 git log > ..\Sources\BIN\Release\Changelog.txt
 dos2unix --u2d ..\Sources\BIN\Release\Changelog.txt
-
+makensis "CleanRegistry.nsi"
 makensis "BUSMASTER INSTALL SCRIPT.nsi"
+
+:END
+exit 0
